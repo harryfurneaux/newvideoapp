96bfb4bdf94905cee135ce50297a85ad302154fa
softmax0112 <softmax0112@gmail.com>
2023-12-11 05:20:05 -0500

    fix: setting menu overlay

--- client/src/components/Modals/account_security.tsx
+++ client/src/components/Modals/account_security.tsx
@@ -10,14 +10,14 @@ const AccountSecurity = ({ show, handleClose }: { show: boolean, handleClose: an
           <div className="text-center mb-4">
             <h5>Account & Security</h5>
           </div>
-          <div className="overlay-part my-3 row align-items-center">
+          <div className="modal-part my-3 row align-items-center">
             <img className="profile-img p-0" src={profile_pic} />
             <div className="col">
               <button className="mb-1">Change Profile Picture</button>
               <p className="pic-desc fw-light">Must be JPEG, PNG, or GIF and cannot exceed 10MB.</p>
             </div>
           </div>
-          <div className="overlay-part my-3 p-2 row align-items-center">
+          <div className="modal-part my-3 p-2 row align-items-center">
             <div className="col">
               <p>NAME</p>
               <p className="fw-light">Full Name</p>
@@ -26,7 +26,7 @@ const AccountSecurity = ({ show, handleClose }: { show: boolean, handleClose: an
               <button>Edit</button>
             </div>
           </div>
-          <div className="overlay-part my-3 p-2 row align-items-center">
+          <div className="modal-part my-3 p-2 row align-items-center">
             <div className="col">
               <p>ORGANISATION</p>
               <p className="fw-light">ORGANISATION</p>
@@ -35,7 +35,7 @@ const AccountSecurity = ({ show, handleClose }: { show: boolean, handleClose: an
               <button>Edit</button>
             </div>
           </div>
-          <div className="overlay-part my-3 p-2 row align-items-center">
+          <div className="modal-part my-3 p-2 row align-items-center">
             <div className="col">
               <p>LOCATION</p>
               <p className="fw-light">Full location</p>
@@ -44,7 +44,7 @@ const AccountSecurity = ({ show, handleClose }: { show: boolean, handleClose: an
               <button>Edit</button>
             </div>
           </div>
-          <div className="overlay-part my-3 p-2 row align-items-center">
+          <div className="modal-part my-3 p-2 row align-items-center">
             <div className="col">
               <p>EMAIL</p>
               <p className="fw-light">**********@emailaddress.com</p>
@@ -53,13 +53,13 @@ const AccountSecurity = ({ show, handleClose }: { show: boolean, handleClose: an
               <button>Edit</button>
             </div>
           </div>
-          <div className="overlay-part my-3 p-2 row align-items-center">
+          <div className="modal-part my-3 p-2 row align-items-center">
             <div className="col">
               <p>PASSWORD</p>
               <p className="fw-light">**********</p>
             </div>
-            <div className="col-2">
-              <button>Edit</button>
+            <div className="col d-flex justify-content-end">
+              <button>Change Password</button>
             </div>
           </div>
           <div className="d-flex justify-content-end">
