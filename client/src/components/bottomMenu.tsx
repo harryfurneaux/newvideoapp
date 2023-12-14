add2b5bcdf96cc81f0ef847a00cf5459c4832122
softmax0112 <softmax0112@gmail.com>
2023-12-14 12:32:12 -0500

    fix: active color on the bottom menu

--- client/src/components/bottomMenu.tsx
+++ client/src/components/bottomMenu.tsx
@@ -4,21 +4,20 @@ const BottomMenu=({mainScreen,setMainScreen}:{mainScreen:number,setMainScreen:an
     return <div className='kdnklms-awendwd'>
         <div onClick={()=>{
             setMainScreen(1)
-        }} className='khjn0-jandw'>
+        }} className={`khjn0-jandw ${mainScreen==1?'active':''}`}>
             <Icons iconNumber={53}/>
             <h5>Questions</h5>
         </div>
         <div onClick={()=>{
             setMainScreen(2)
-        }}  className='khjn0-jandw'>
+        }} className={`khjn0-jandw ${mainScreen==2?'active':''}`}>
             <Icons iconNumber={54}/>
             <h5 className=''>Answers</h5>
         </div>
         <div 
         onClick={()=>{
             setMainScreen(3)
-        }} 
-        className='khjn0-jandw'>
+        }} className={`khjn0-jandw ${mainScreen==3?'active':''}`}>
             <Icons iconNumber={55}/>
             <h5>Messages</h5>
         </div>
