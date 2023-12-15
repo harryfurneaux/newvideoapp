a23e860499df1023874ca5d9b9138eeed0e8380a
softmax0112 <softmax0112@gmail.com>
2023-12-15 16:21:58 -0500

    fix: answers leftside screen

--- client/src/components/Answers/MainForm.tsx
+++ client/src/components/Answers/MainForm.tsx
@@ -1,30 +1,35 @@
+import { Container, Row, Col } from "react-bootstrap";
 import Card from "./Card";
 
-const MainForm = ({showScreen,setshowScreen}:{showScreen:number,setshowScreen:any}) => {
+const MainForm = ({ showScreen, setshowScreen }: { showScreen: number, setshowScreen: any }) => {
   return (
     <div className="leftSideMain">
       <div className="kjkdsjnds-aned">
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showFav showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
-        <Card showScreen={showScreen} setshowScreen={setshowScreen}/>
+        <Container>
+          <Row className="row-cols-3 row-cols-sm-4 row-cols-md-4 row-cols-lg-5 row-cols-xl-6">
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showFav showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+            <Col><Card showScreen={showScreen} setshowScreen={setshowScreen} /></Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
